c407d9b27dc2df7b5aa9b4d37a89924658ca553b
Lopera <loperilladev@gmail.com>
2024-11-15 16:51:11 +0100

    add theme
    Signed-off-by: Manuel Lopera <loperilladev@gmail.com>

--- app/src/main/java/io/loperilla/jokeapp/presentation/MainActivity.kt
+++ app/src/main/java/io/loperilla/jokeapp/presentation/MainActivity.kt
@@ -4,32 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import io.loperilla.jokeapp.presentation.navigator.Navigator
 import io.loperilla.jokeapp.presentation.screens.AppNavigation
 import io.loperilla.jokeapp.presentation.theme.JokeAppTheme
 import org.koin.android.ext.android.inject
-import org.koin.compose.KoinContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             JokeAppTheme {
-                KoinContext {
-                    val navigator: Navigator by inject()
-                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                        AppNavigation(
-                            navigator = navigator,
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(innerPadding)
-                        )
-                    }
-                }
+                val navigator: Navigator by inject()
+                AppNavigation(
+                    navigator = navigator,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.surface)
+                )
             }
         }
     }
